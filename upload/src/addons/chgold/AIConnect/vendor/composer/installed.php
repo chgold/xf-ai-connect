ead56f2cd6cc9df449017f9e5cc2ff5d7ab07dc2
chagold <205388-chagold@users.noreply.drupalcode.org>
2026-03-12 02:24:53 +0200

    release: v1.1.10

---
+++ upload/src/addons/chgold/AIConnect/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'chgold/xenforo-ai-connect',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => null,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'chgold/xenforo-ai-connect' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => null,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
